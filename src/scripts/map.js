4ed2d9ba916571a23ca65c1342b5aacf513335f6
Augustine Opoku-Kesse <augustine.opoku-kesse@amalitech.org>
2022-03-11 03:12:41 +0100

    Complete google map integration

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -1,12 +1,40 @@
 function initMap() {
-    let location = {lat: 4.909456, lng: -1.755881};
+    let location = {lat: 8.0300028, lng: -1.080027};
+    let specificLocation = {lat: 4.909456, lng: -1.755881};
+    let viewButtonOne  = document.getElementById('view-button-one');
+    let viewButtonTwo  = document.getElementById('view-button-two');
     let map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 15,
+        zoom: 5,
         center: location
     });
 
-    let markerVarOne = new google.maps.Marker({
-        position: location,
+    const contentString = '<h3 class="dark-blue">Training Center</h3>';
+    const infoWindowOne = new google.maps.InfoWindow({
+        content: contentString
+    });
+    
+
+    let marker = new google.maps.Marker({
+        position: specificLocation,
         map: map
     });
+
+    marker.addListener("click", () => {
+        infoWindowOne.open(map, marker);
+    });
+
+    marker.addListener("click", () => {
+        map.setZoom(16);
+        map.setCenter(marker.getPosition());
+    });
+
+    viewButtonOne.addEventListener('click', () => {
+        map.setZoom(16);
+        map.setCenter(marker.getPosition());
+    });
+    viewButtonTwo.addEventListener('click', () => {
+        map.setZoom(16);
+        map.setCenter(marker.getPosition());
+    });
+
 }
